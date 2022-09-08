460fed81f6ca49dac729c9c75914fbc53a64be9e
Leuow95 <leuow95@gmail.com>
2022-09-07 23:08:30 -0400

    added keep watching button

---
+++ design_system/lib/components/buttons/fm_home_keep_watching_button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class KeepWatchingButton extends StatelessWidget {
+  final String title;
+
+  const KeepWatchingButton({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
