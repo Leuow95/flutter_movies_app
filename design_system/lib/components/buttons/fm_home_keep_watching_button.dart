e1edddcfe5b1b8c2846ff46152d78886800c26c9
Leuow95 <leuow95@gmail.com>
2022-09-08 23:20:45 -0400

    refactored fm_home_keepwatching

--- design_system/lib/components/buttons/fm_home_keep_watching_button.dart
+++ design_system/lib/components/buttons/fm_home_keep_watching_button.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 class KeepWatchingButton extends StatelessWidget {
   final String title;
 
-  const KeepWatchingButton({Key? key, required this.title}) : super(key: key);
+  const KeepWatchingButton({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: ElevatedButton(
+        onPressed: () => {},
+        child: Icon(Icons.play_arrow),
+      ),
+    );
   }
 }
