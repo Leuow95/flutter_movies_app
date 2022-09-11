304ec6ed48d61c43feb593008d656f5b7f63632e
Leuow95 <leuow95@gmail.com>
2022-09-11 15:05:30 -0400

    added a listener to pageview

--- design_system/lib/components/carousel_slider/fm_page_view.dart
+++ design_system/lib/components/carousel_slider/fm_page_view.dart
@@ -1,3 +1,4 @@
+import 'package:design_system/components/images/fm_movie_image.dart';
 import 'package:flutter/material.dart';
 
 class FMCarouselSlider extends StatefulWidget {
@@ -8,31 +9,36 @@ class FMCarouselSlider extends StatefulWidget {
 }
 
 class _FMCarouselSliderState extends State<FMCarouselSlider> {
+  final pageController = PageController(viewportFraction: 0.75);
+  int currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      int position = pageController.page!.round();
+      if (currentPage != position) {
+        setState(() {
+          currentPage = position;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
       itemCount: 8,
       physics: BouncingScrollPhysics(),
       itemBuilder: ((context, index) {
-        return Container(
-          color: Colors.green,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: AnimatedContainer(
-              duration: Duration(milliseconds: 500),
-              curve: Curves.ease,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.red,
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.black87,
-                        blurRadius: 30,
-                        offset: Offset(30, 20))
-                  ]),
-            ),
-          ),
-        );
+        final bool active = index == currentPage;
+        return MovieImage(active);
       }),
     );
   }
