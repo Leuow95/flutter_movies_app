041dc02efb7b12f7f610b170c095f97272d4eb52
Leuow95 <leuow95@gmail.com>
2022-09-11 14:44:20 -0400

    updated carousel page view

--- design_system/lib/components/carousel_slider/fm_page_view.dart
+++ design_system/lib/components/carousel_slider/fm_page_view.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
 
-class FMCarouselSlider extends StatelessWidget {
+class FMCarouselSlider extends StatefulWidget {
   const FMCarouselSlider({Key? key}) : super(key: key);
 
   @override
+  State<FMCarouselSlider> createState() => _FMCarouselSliderState();
+}
+
+class _FMCarouselSliderState extends State<FMCarouselSlider> {
+  @override
   Widget build(BuildContext context) {
     return PageView.builder(
-      itemCount: 3,
-      itemBuilder: ((context, index) => Container(
-            color: Colors.green,
-            child: Center(
-              child: Text("Page View funcionando"),
+      itemCount: 8,
+      physics: BouncingScrollPhysics(),
+      itemBuilder: ((context, index) {
+        return Container(
+          color: Colors.green,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 500),
+              curve: Curves.ease,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.red,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black87,
+                        blurRadius: 30,
+                        offset: Offset(30, 20))
+                  ]),
             ),
-          )),
+          ),
+        );
+      }),
     );
   }
 }
