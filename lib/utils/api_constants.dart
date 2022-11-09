a72149cd9342e07abe9944635c1d352fcc324aae
leonardomaia <leuow95@gmail.com>
2022-11-09 17:23:53 -0400

    feat: ceated api constants

---
+++ lib/utils/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static const apiKey = "?api_key=ec07490ca7718f6d270e0e731a5481ce";
+  static const baseUrl = "https://api.themoviedb.org/3/movie/popular";
+  static const popular = "/popular";
+}
