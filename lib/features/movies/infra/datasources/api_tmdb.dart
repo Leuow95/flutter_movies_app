28ed15f08341a0cdaf8be6c17b7cfd21a723b43d
leonardomaia <leuow95@gmail.com>
2022-11-09 17:22:51 -0400

    feat: changed datasource params

--- lib/features/movies/infra/datasources/api_tmdb.dart
+++ lib/features/movies/infra/datasources/api_tmdb.dart
@@ -1,5 +1,8 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_movies_app/features/movies/infra/models/movie_model.dart';
 
+import '../../domain/failures/movies_failure.dart';
+
 abstract class ApiMoviesDataSource {
-  Future<List<MovieModel>> getPopularMovies();
+  Future<Either<DataSourceError, List<MovieModel>>> getPopularMovies();
 }
